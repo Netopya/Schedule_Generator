f9dba041be3f2558a6475faa8c4811bced3d625c
Michael B <netopya@netopyaplanet.com>
2016-09-10 20:34:13 -0400

    Added table to hold and edit items

--- js/script.js
+++ js/script.js
@@ -1,39 +1,72 @@
 var data = [
     {
         "className": "CompSci",
-        "classType": "Lecture",
-        "classStartTime": "8:00AM",
-        "classEndTime": "10:00AM",
+        "classType": 1,
+        "classStartTime": "10:00AM",
+        "classEndTime": "12:00AM",
         "classLocation": "FG 9000"
     }
 ];
 
-
-
 $(function () {
     $('#table').bootstrapTable({
         data: data,
+        toggle: "table",
         columns: [
             {
                 field: "className",
                 title: "Name",
+                editable: true,
+                align: 'center'
             },
             {
                 field: "classType",
                 title: "Type",
+                editable: {
+                    type: "select",
+                    value: 1,    
+                    source: [
+                        {value: 1, text: 'Lecture'},
+                        {value: 2, text: 'Tutorial'},
+                        {value: 3, text: 'Laboratory'}
+                    ]
+                }
             },
             {
                 field: "classStartTime",
                 title: "Start Time",
+                editable: {
+                    type: "combodate",
+                    format: 'h:mm a',    
+                    viewformat: 'h:mm a',    
+                    template: 'h : mm a',    
+                    combodate: {
+                        minuteStep: 15
+                    }
+                }
             },
             {
                 field: "classEndTime",
                 title: "End Time",
+                editable: {
+                    type: "combodate",
+                    format: 'h:mm a',    
+                    viewformat: 'h:mm a',    
+                    template: 'h : mm a',    
+                    combodate: {
+                        minuteStep: 15
+                    }
+                }
             },
             {
                 field: "classLocation",
                 title: "Location",
+                editable: true
             }
         ]
     });
 });
+
+function locationFormatter() {
+
+}
